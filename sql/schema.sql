9c3db24dbdd9830ea8ef6eb74dc25aabdc58bf98
kyb <js@szpilewski.com>
2017-06-18 17:25:15 +0200

    inserting works

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,22 +1,27 @@
 BEGIN TRANSACTION;
 
+	-- exchanges
 	CREATE TABLE exchanges(
 		id integer primary key,
 		name text NOT NULL,
 		url text NOT NULL);
 
+	-- prefill exchanges
+	INSERT INTO "exchanges" VALUES(1, "Poloniex", "https://poloniex.com");
+	INSERT INTO "exchanges" VALUES(2, "Bittrex", "https://bittrex.com");
+	INSERT INTO "exchanges" VALUES(3, "Kraken", "https://kraken.com");
+
+	
+	-- transactions
 	CREATE TABLE transactions(
 		id integer primary key, 
 		action text NOT NULL,
 		pair text NOT NULL,
 		amount number NOT NULL,
 		price number NOT NULL,
-		timestamp integer NOT NULL,
+		timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 		exchange_id integer references exchanges(id) NOT NULL
 	);
 
-
-	INSERT INTO "exchanges" VALUES(1, "Poloniex", "https://poloniex.com");
-	INSERT INTO "exchanges" VALUES(2, "Bittrex", "https://bittrex.com");
 
 COMMIT;
