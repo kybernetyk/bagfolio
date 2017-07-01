3b4ebb242fd3baca3b5fa03d1ff9dba6f0f65d20
kyb <js@szpilewski.com>
2017-07-01 14:36:44 +0200

    cronjob now runs on my freebsd machine

--- report_job.sh
+++ report_job.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-. /Users/kyb/.zshrc
+. /home/kyb/.zshrc
 
 # User variables
 email=js@szpilewski.com
@@ -8,7 +8,7 @@ fn=/tmp/short.log
 
 
 # Run backup
-cd /Users/kyb/code/bagfolio
+cd /home/kyb/src/bagfolio
 ./report > $fn
 
 mail -s "$subject" $email < $fn
